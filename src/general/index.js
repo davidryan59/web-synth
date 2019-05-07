d6ef2a0143525f4e83f86e0c8c19d4f253b9e9e2
David Ryan <davidryan1998@hotmail.com>
2019-05-07 08:07:23 +0700

    Slider value in store now numeric

--- src/general/index.js
+++ src/general/index.js
@@ -1,4 +1,4 @@
 // Make string exactly N (len) characters
 // If too short, put non-breaking spaces in front
 // If too long, truncate on right.
-export const makeNChars = (label, len) => label.slice(0, len).padStart(len, '\u00a0')
+export const makeNChars = (label, len) => (''+label).slice(0, len).padStart(len, '\u00a0')
