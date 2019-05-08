2a8b153ebea39e903e980e1368a4b896545406a2
David Ryan <davidryan1998@hotmail.com>
2019-05-08 09:31:21 +0700

    Used thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import AppC from './components/AppC'
 import rootReducer from './reducers'
 
 import windowResizeHandler from './handlers/windowResizeHandler'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 window.addEventListener('resize', e => windowResizeHandler(e, store))
 
