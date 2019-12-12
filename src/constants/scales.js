5ddd2f46d2a7eb43e037474b1c524db348e36be5
David Ryan <davidryan1998@hotmail.com>
2019-12-12 14:10:16 +0700

    Scales updated

--- src/constants/scales.js
+++ src/constants/scales.js
@@ -4,12 +4,14 @@ export const scaleSetupArray = []
 
 scaleSetupArray.push(['Major Scale', [24, 27, 30, 32, 36, 40, 45]])
 scaleSetupArray.push(['Major Chord', [4, 5, 6]])
-scaleSetupArray.push(['Pentatonic (Major 9th)', [5, 6, 7, 8, 9]])
-scaleSetupArray.push(['Pentatonic (7-limit)', [12, 14, 16, 18, 21]])
-scaleSetupArray.push(['11-limit', [6, 7, 8, 9, 10, 11]])
-scaleSetupArray.push(['13-limit', [7, 8, 9, 10, 11, 12, 13]])
+scaleSetupArray.push(['Minor Chord', [10, 12, 15]])
+scaleSetupArray.push(['Pentatonic (Dom 7th)', [8, 9, 10, 12, 14]])
+scaleSetupArray.push(['Pentatonic (Maj 7th)', [8, 9, 10, 12, 15]])
+scaleSetupArray.push(['Pentatonic (7-limit)', [16, 18, 21, 24, 28]])
+scaleSetupArray.push(['11-limit', [8, 9, 10, 11, 12, 14]])
+scaleSetupArray.push(['13-limit', [8, 9, 10, 11, 12, 13, 14]])
 scaleSetupArray.push(['8 to 16', [8, 9, 10, 11, 12, 13, 14, 15]])
-scaleSetupArray.push(['17-limit', [9, 10, 11, 12, 13, 14, 15, 16, 17]])
+scaleSetupArray.push(['19-limit', [16, 17, 18, 19, 20, 22, 24, 26, 28, 30]])
 scaleSetupArray.push(['16 to 32', [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]])
 scaleSetupArray.push(['32 to 64', [
   32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
